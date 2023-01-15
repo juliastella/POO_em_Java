12869921d2f8de1753a37c36a3562f32150c5d0c
juliastella <juliastellasil1@gmai.com>
2023-01-15 20:30:18 -0300

    Trabalhando collections, parte 01

--- src/model/ContaBancaria.java
+++ src/model/ContaBancaria.java
@@ -1,15 +1,17 @@
 package src.model;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.InputMismatchException;
 
-public  abstract class ContaBancaria {
+public abstract class ContaBancaria {
     // #region atributos
     private String agencia;
     private String conta;
     private Integer digito;
     private Double saldo;
     private Date dataAbertura; // Usando o arquivo utils
+    private ArrayList<Movimentacao> movimentacoes; // Podem ter varias movimentações, já que um array.
     private Double VALOR_MINIMO_DEPOSITO = 10.0; // Isso é a declaração de uma constante.
     // #endregion
 
@@ -20,6 +22,16 @@ public  abstract class ContaBancaria {
         this.digito = digito;
         this.saldo = saldoInicial;
         this.dataAbertura = new Date(); // vai pegar data e hora do pc e vai usala.
+
+        // Se não instanciar, vai dar uma excpetion de nullPointerException.
+        this.movimentacoes = new ArrayList<Movimentacao>();
+
+        // Criando a primeira movimentação
+        Movimentacao movimentacao = new Movimentacao("Abertura de conta", saldoInicial);
+
+        // Agora vai salvar a movimentação a cima dentro do array e movimentações.
+        // Aqui está iniciando o meu extrato bancario.
+        this.movimentacoes.add(movimentacao);
     }
     // #endregion
 
@@ -51,10 +63,12 @@ public  abstract class ContaBancaria {
     public Double getSaldo() {
         return saldo;
     }// Não vai existi um set para saldo, pois saldo não vai ser alterado
+
     public Date getDataAbertura() {
         return dataAbertura;
-    }// Não vai existir, pois a data vai ser no momento em que estiver construindo a mesma.
-    // #endregion
+    }// Não vai existir, pois a data vai ser no momento em que estiver construindo a
+     // mesma.
+     // #endregion
 
     // #region Metodos
     public void depositar(Double valor) { // adicionar valor a saldo.
@@ -71,22 +85,30 @@ public  abstract class ContaBancaria {
         // Se o valor é maior que dez, então this.saldo adiciona.
         this.saldo += valor;
 
+        // Aqui faço uma movimentação no extrato.
+        Movimentacao movimentacao = new Movimentacao("Deposito", valor);
+        this.movimentacoes.add(movimentacao);
     }
 
     public Double sacar(Double valor) {
-       // Verifica se o valor é maior que o saldo da conta.
-       // Se for manda mensagem de saldo insuficiente.
-       if(valor > this.saldo){
-         throw new InputMismatchException("O saldo é insuficiente");
-       }
-       
-       // Aqui removemos o valor de saque do saldo atual.
-       this.saldo -= valor;
-       // Retorna o valor sacado ao usuario.
-       return valor;
+        // Verifica se o valor é maior que o saldo da conta.
+        // Se for manda mensagem de saldo insuficiente.
+        if (valor > this.saldo) {
+            throw new InputMismatchException("O saldo é insuficiente");
+        }
+
+        // Aqui removemos o valor de saque do saldo atual.
+        this.saldo -= valor;
+
+        // Aqui faço uma movimentação no extrato.
+        Movimentacao movimentacao = new Movimentacao("Retirada de valor", valor);
+        this.movimentacoes.add(movimentacao);
+
+        // Retorna o valor sacado ao usuario.
+        return valor;
     }
 
-    public void transferir(Double valor, ContaBancaria contaDestino){
+    public void transferir(Double valor, ContaBancaria contaDestino) {
         // Efetua um saque na conta atual
         this.sacar(valor); // encapsulado o metodo sacar, já que transferir usara a mesma fução.
         contaDestino.depositar(valor); // Vai pegar a operação do saque armazenar em contaDestino e aplicar o metodo
